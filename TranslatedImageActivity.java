29c1cc3c7b0cf1b393f9ef8e1392b951d30f44e2
WhiteTorte <164726397+WhiteTorte@users.noreply.github.com>
2024-05-31 10:12:06 +0900

    Add files via upload

---
+++ TranslatedImageActivity.java
@@ -0,0 +1,23 @@
+package com.mtsahakis.mediaprojectiondemo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TranslatedImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translated_image);
+
+        String imagePath = getIntent().getStringExtra("translatedImagePath");
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+
+        ImageView imageView = findViewById(R.id.translatedImageView);
+        imageView.setImageBitmap(bitmap);
+    }
+}
